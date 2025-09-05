253c2d441c9295da1be2ac3b22d89b1942bb6ecc
yaku555 <javierhermosilla17@gmail.com>
2025-09-04 22:59:37 -0400

    Se pasó el proyecto a React

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export function Inicio() {
+  return (
+ 
+      <h1>Bienvenido a la página de inicio</h1>
+
+  );
+}
+
+
+export function Denunciar() {
+  return <div><h3>test1</h3><p>test</p></div>;
+}
+
+export function Mi_Siniestro() {
+  return <div><h3>test2</h3><p>test</p></div>;
+}
+
+export function Talleres() {
+  return <div><h3>test3</h3><p>test</p></div>;
+}
+
+// 👇 Este sigue siendo tu único export default
+export default function App() {
+  return (
+    <Router>
+      <nav className="navbar">
+        <ul>
+          <li><Link to="/">INICIO</Link></li>
+          <li><Link to="/denunciar">DENUNCIAR</Link></li>
+          <li><Link to="/mi_siniestro">MI SINIESTRO</Link></li>
+          <li><Link to="/talleres">TALLERES</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/denunciar" element={<Denunciar />} />
+          <Route path="/mi_siniestro" element={<Mi_Siniestro />} />
+          <Route path="/talleres" element={<Talleres />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
